7e4383f0fc34538f3739c1bdd6c9beb3aaa67c63
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2018-07-11 19:19:19 -0400

    backend setup

--- martingale_node_backend/app.js
+++ martingale_node_backend/app.js
@@ -25,7 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-// app.use('/games', gamesRouter);
 app.use('/games', gamesRouter);
 app.use('/users', usersRouter);
 app.use('/spins', spinsRouter);
