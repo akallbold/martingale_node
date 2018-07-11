ffad0df0fc52038eaec76493f5ca846d84c2d919
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2018-07-11 14:37:33 -0400

    chart now shows lifetime

--- src/MainContainer.js
+++ src/MainContainer.js
@@ -45,30 +45,18 @@ class MainContainer extends React.Component {
     let probOfWin
     let winsNeededForGoal = Math.ceil(this.state.goal/this.state.bet)
     let lossesNeededForGameOver = Math.floor(this.getBaseLog(2,(this.state.maxInvestment/this.state.bet)))
-    //now we calculate the probability that we will spin our wins needed before we spin our losses needed IN A ROW.
-
-    //first we find the probability of losing x times in a row (resulting in losing our maxInvestment)
     let probOfLosingInARow= Math.pow(.53,lossesNeededForGameOver)
-
-    //given probOfLosingInARow, we can find the median number of spins needed to reach probOfLosingInARow. We are using probOfLosingInARow here instead of probOfLosingOnce (53%) and demonstrating that we only need to experience probOfLosingInARow once to replicate the "lose x times in a row scenario"
     let averageSpinsForLoss = 1/probOfLosingInARow
-
-    //we now have the average spins for a loss and need to calculate, what is the probability that we will win winNeededForGoal times before we hit the average spins needed to result in a loss.
-
-    //using negative binomial distribution we can find the probability that we will win the game in a specific spin. We can then sum these probabilities to get the probability that we will spin enough wins to win the game before we get to the average amount of times it takes to lose the game. If our averageSpinsForLoss is less than our winsNeededForGoal then we find the inverse (1-probability).
-
     if (winsNeededForGoal > averageSpinsForLoss){
       probOfWin = 1
       for (let i = 0; i<=winsNeededForGoal; i++){
         probOfWin -= (this.factorial(i-1)/(this.factorial((i-1)-(winsNeededForGoal-1))*this.factorial(winsNeededForGoal-1))) * Math.pow(.47,winsNeededForGoal) * Math.pow(.53,(i-winsNeededForGoal))
       }
-        // this.setState({probOfWin:probOfWin})
     } else {
       probOfWin = 0
       for (let i = winsNeededForGoal; i <= averageSpinsForLoss; i++){
         probOfWin += (this.factorial(i-1)/(this.factorial((i-1)-(winsNeededForGoal-1))*this.factorial(winsNeededForGoal-1))) * Math.pow(.47,winsNeededForGoal) * Math.pow(.53,(i-winsNeededForGoal))
       }
-        // this.setState({probOfWin:probOfWin})
     }
     return probOfWin
   }
@@ -181,6 +169,7 @@ class MainContainer extends React.Component {
   }
 
   fetchGetGameResults = () => {
+    console.log("fetching game results")
     fetch('http://localhost:3000/games', {
       headers: {"Access-Control-Allow-Origin":'*',
                 "Content-Type": "application/json"
@@ -193,6 +182,8 @@ class MainContainer extends React.Component {
   }
 
   fetchGetSpinResults = () => {
+    console.log("fetching spin results")
+
     fetch('http://localhost:3000/spins', {
       headers: {"Access-Control-Allow-Origin":'*',
                 "Content-Type": "application/json"
@@ -240,12 +231,14 @@ class MainContainer extends React.Component {
   lifetimeRBGStats = () => {
     let output = {red: 0, black: 0, green: 0}
 
-    this.state.gameStats.forEach(game => {
-      game.currentGameSpins.forEach(spin => {
-        output[spin[0].resultCol]++
-      })
+    this.state.lifetimeSpinStats.forEach(spin => {
+        output[spin.spinColResult]++
     })
-    // console.log(output)
+      this.state.gameStats.forEach(game => {
+        game.currentGameSpins.forEach(spin => {
+          output[spin[0].resultCol]++
+        })
+      })
     return output
   }
 
@@ -264,8 +257,6 @@ class MainContainer extends React.Component {
   }
 
   render(){
-    console.log("lifetime game", this.state.lifetimeGameStats)
-    console.log("lifetime spin", this.state.lifetimeSpinStats)
     return (
       <div className= "container">
         <div className= "row">
